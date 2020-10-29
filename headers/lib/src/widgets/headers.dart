19a8dd1951792e3bcb02052d6b539e8856eebcab
Kike Sanchez <kikin1601@hotmail.com>
2020-10-29 07:46:31 -0500

    Header diagonal

--- headers/lib/src/widgets/headers.dart
+++ headers/lib/src/widgets/headers.dart
@@ -24,3 +24,45 @@ class HeaderBordeRedondeado extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615aab),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    //propiedades
+    lapiz.color = Color(0xff615aab);
+    lapiz.style = PaintingStyle.fill; //.stroke
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    //Dibujar con el path y el lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    //path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
