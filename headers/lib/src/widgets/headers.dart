11ad5dbd59c04947976fd3e4db1c907cea350090
Kike Sanchez <kikin1601@hotmail.com>
2020-10-29 08:19:55 -0500

    Header Waves

--- headers/lib/src/widgets/headers.dart
+++ headers/lib/src/widgets/headers.dart
@@ -192,3 +192,45 @@ class _HeaderCurvoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615aab),
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = new Paint();
+
+    //propiedades
+    lapiz.color = Color(0xff615aab);
+    lapiz.style = PaintingStyle.fill; //.stroke
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.3);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.35,
+        size.width * 0.5, size.height * 0.3);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.25, size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
