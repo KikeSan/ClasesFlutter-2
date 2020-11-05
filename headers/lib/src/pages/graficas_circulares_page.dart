dd8d4274f9679f0736193405bd1cd4bd6d21ee69
Kike Sanchez <kikin1601@hotmail.com>
2020-11-05 07:14:16 -0500

    Dibujar el circulo y el arco

--- headers/lib/src/pages/graficas_circulares_page.dart
+++ headers/lib/src/pages/graficas_circulares_page.dart
@@ -24,8 +24,8 @@ class _GraficasCircularesPageState extends State<GraficasCircularesPage> {
         child: Container(
             width: 300,
             height: 300,
-            color: Colors.red,
-            child: RadialProgress(porcentaje: 40.0,),
+            //color: Colors.red,
+            child: RadialProgress(porcentaje: porcentaje,),
             //child: Text('$porcentaje %', style: TextStyle(fontSize: 50.0),)),
       ),
     ));
--- headers/lib/src/widgets/radial_progress.dart
+++ headers/lib/src/widgets/radial_progress.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RadialProgress extends StatefulWidget {
@@ -14,8 +16,56 @@ class _RadialProgressState extends State<RadialProgress> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('${widget.porcentaje}'),
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _MiRadialProgress(widget.porcentaje),
+      ),
     );
   }
+}
+
+
+class _MiRadialProgress extends CustomPainter{
+  final porcentaje;
+
+  _MiRadialProgress(this.porcentaje);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+
+
+    final paint = new Paint()
+      ..strokeWidth = 4
+      ..color = Colors.black12
+      ..style = PaintingStyle.stroke;
+
+    final center = new Offset(size.width*0.5, size.height/2);
+    final radio = min(size.width*0.5, size.height*0.5);
+
+    canvas.drawCircle(center, radio, paint);
+
+    //arco
+    final paintArco = new Paint()
+      ..strokeWidth = 10
+      ..color = Colors.redAccent[700]
+      ..style = PaintingStyle.stroke;
+
+    //parte q se va llenando
+    double arcAngle = 2*pi*(porcentaje/100);
+    canvas.drawArc(
+        Rect.fromCircle(center: center,radius: radio),
+        -pi/2,
+        arcAngle,
+        false,
+        paintArco);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
 }
 
