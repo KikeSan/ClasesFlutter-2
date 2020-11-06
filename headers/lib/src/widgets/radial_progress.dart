9f53535165d57a970e774fda1017ee5df74555a9
Kike Sanchez <kikin1601@hotmail.com>
2020-11-06 07:40:05 -0500

    Gradiente stroke

--- headers/lib/src/widgets/radial_progress.dart
+++ headers/lib/src/widgets/radial_progress.dart
@@ -85,13 +85,17 @@ class _MiRadialProgress extends CustomPainter{
 
   @override
   void paint(Canvas canvas, Size size) {
-
-
+    final Rect rect = new Rect.fromCircle(
+      center: Offset(0,0),
+      radius: 180
+    );
+    final Gradient gradiente = new LinearGradient(
+      colors: <Color>[Color(0xffc012ff),Color(0xff6d05e8),Colors.red]
+    );
 
     final paint = new Paint()
       ..strokeWidth = grosorSecundario
       ..color = colorSecundario
-      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
     final center = new Offset(size.width*0.5, size.height/2);
@@ -102,7 +106,9 @@ class _MiRadialProgress extends CustomPainter{
     //arco
     final paintArco = new Paint()
       ..strokeWidth = grosorPrimario
-      ..color = colorPrimario
+      //..color = colorPrimario
+      ..shader = gradiente.createShader(rect)
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
     //parte q se va llenando
