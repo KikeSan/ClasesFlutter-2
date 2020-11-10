2ea7c9c4ee66fc2e2cecded3ce6e47475eec3f0c
Kike Sanchez <kikin1601@hotmail.com>
2020-11-10 07:38:19 -0500

    configuraciones usando Provider

--- headers/lib/src/pages/slideshow_page.dart
+++ headers/lib/src/pages/slideshow_page.dart
@@ -1,116 +1,24 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:headers/src/widgets/slideshow.dart';
 
-class SlideShowPage extends StatelessWidget {
+class SlideshowPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                child: _Slides()),
-            _Dots()
-          ],
-        )
-      ),
-    );
-  }
-}
-
-class _Dots extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 80,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _Dot(0),
-          _Dot(1),
-          _Dot(2)
+      backgroundColor: Colors.purple[50],
+      body: Slideshow(
+        //puntosArriba: true,
+        colorPrimario: Colors.purple,
+        colorSecundario: Colors.purple[200],
+        slides: <Widget>[
+          SvgPicture.asset('assets/svgs/slide-1.svg'),
+          SvgPicture.asset('assets/svgs/slide-2.svg'),
+          SvgPicture.asset('assets/svgs/slide-3.svg'),
+          SvgPicture.asset('assets/svgs/slide-4.svg'),
+          SvgPicture.asset('assets/svgs/slide-5.svg'),
         ],
-      ),
+      )
     );
   }
 }
-
-class _Dot extends StatelessWidget {
-  final int index;
-
-  _Dot(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 15,
-      height: 15,
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      decoration: BoxDecoration(
-        color:Colors.black26,
-        shape: BoxShape.circle
-      ),
-    );
-  }
-}
-
-
-class _Slides extends StatefulWidget {
-
-  @override
-  __SlidesState createState() => __SlidesState();
-}
-
-class __SlidesState extends State<_Slides> {
-  final pageViewController = new PageController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    pageViewController.addListener(() {
-
-    });
-  }
-  @override
-  void dispose(){
-    pageViewController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-
-      child: PageView(
-        controller: pageViewController,
-        children: <Widget>[
-          _Slide('assets/svgs/slide-1.svg'),
-          _Slide('assets/svgs/slide-2.svg'),
-          _Slide('assets/svgs/slide-3.svg'),
-        ],
-      ),
-    );
-  }
-}
-
-class _Slide extends StatelessWidget {
-  final String svg;
-  _Slide(this.svg);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      padding: EdgeInsets.all(30),
-      child: SvgPicture.asset(svg),
-    );
-  }
-}
-
-
-
-
---
+++ headers/lib/src/widgets/slideshow.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+//import 'package:headers/src/models/slider_model.dart';
+import 'package:provider/provider.dart';
+
+//import 'package:flutter_svg/svg.dart';
+
+class Slideshow extends StatelessWidget {
+
+  final List<Widget> slides;
+  final bool puntosArriba;
+  final Color colorPrimario;
+  final Color colorSecundario;
+
+  Slideshow({
+    @required this.slides,
+    this.puntosArriba = false,
+    this.colorPrimario = Colors.blue,
+    this.colorSecundario = Colors.black26
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => new _SlidershowModel(),
+      child: SafeArea(
+        child: Center(
+          child: Builder(
+            builder: (BuildContext context){
+              Provider.of<_SlidershowModel>(context).colorPrimario = this.colorPrimario;
+              Provider.of<_SlidershowModel>(context).colorSecundario = this.colorSecundario;
+              return _CrearEstructuraSlideshow(puntosArriba: puntosArriba, slides: slides);
+            },
+          ),
+      ),
+    ),
+    );
+  }
+}
+
+class _CrearEstructuraSlideshow extends StatelessWidget {
+  const _CrearEstructuraSlideshow({
+    Key key,
+    @required this.puntosArriba,
+    @required this.slides,
+  }) : super(key: key);
+
+  final bool puntosArriba;
+  final List<Widget> slides;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        if(this.puntosArriba)
+          _Dots(this.slides.length),
+        Expanded(
+            child: _Slides(this.slides)),
+        if(!this.puntosArriba)
+          _Dots(this.slides.length),
+
+      ],
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+
+  final int totalSlides;
+
+  _Dots(this.totalSlides);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 180,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        /*children: [
+          _Dot(0),
+          _Dot(1),
+          _Dot(2)
+        ],*/
+        children: List.generate(this.totalSlides, (index) => _Dot(index)),
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  final int index;
+
+  _Dot(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    //final pageViewIndex = Provider.of<_SlidershowModel>(context).currentPage;
+    final ssModel = Provider.of<_SlidershowModel>(context);
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      width: 15,
+      height: 15,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color:(ssModel.currentPage>=index-0.5 && ssModel.currentPage<index+0.5)
+            ?ssModel.colorPrimario:ssModel.colorSecundario,
+        shape: BoxShape.circle
+      ),
+    );
+  }
+}
+
+
+class _Slides extends StatefulWidget {
+
+  final List<Widget> slides;
+  _Slides(this.slides);
+
+  @override
+  __SlidesState createState() => __SlidesState();
+}
+
+class __SlidesState extends State<_Slides> {
+  final pageViewController = new PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageViewController.addListener(() {
+      Provider.of<_SlidershowModel>(context, listen: false).currentPage = pageViewController.page;
+    });
+  }
+  @override
+  void dispose(){
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: PageView(
+        controller: pageViewController,
+        /*children: <Widget>[
+          _Slide('assets/svgs/slide-1.svg'),
+          _Slide('assets/svgs/slide-2.svg'),
+          _Slide('assets/svgs/slide-3.svg'),
+        ],*/
+        children: widget.slides.map((slide) => _Slide(slide)).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Widget slide;
+  _Slide(this.slide);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: this.slide,
+    );
+  }
+}
+
+
+class _SlidershowModel with ChangeNotifier{
+  double _currentPage = 0;
+  Color _colorPrimario = Colors.blue;
+  Color _colorSecundario = Colors.grey;
+
+
+
+
+  double get currentPage => this._currentPage;
+
+  set currentPage(double value) {
+    this._currentPage = value;
+    notifyListeners();
+  }
+
+  Color get colorPrimario => this._colorPrimario;
+
+  set colorPrimario(Color value) {
+    this._colorPrimario = value;
+    notifyListeners();
+  }
+
+  Color get colorSecundario => this._colorSecundario;
+
+  set colorSecundario(Color value) {
+    this._colorSecundario = value;
+    notifyListeners();
+  }
+}
+
