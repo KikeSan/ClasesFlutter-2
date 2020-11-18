94771ed16866731c1f8b73e26bb9c686e47e24b1
Kike Sanchez <kikin1601@hotmail.com>
2020-11-18 08:58:34 -0500

    Configurando Botones

--- headers/lib/src/pages/emergency_page.dart
+++ headers/lib/src/pages/emergency_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:headers/src/widgets/boton_gordo.dart';
 import 'package:headers/src/widgets/headers.dart';
 
 class EmergencyPage extends StatelessWidget {
@@ -6,7 +8,35 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(),
+      backgroundColor: Colors.grey[100],
+      body: Center(
+          child: BotonGordo(
+            icon: FontAwesomeIcons.carCrash,
+            texto: 'Motor Accident',
+            color1: Color(0xff6989f5),
+            color2: Color(0xff906ef5),
+            onPress: (){
+              print('Click');
+            },
+          )
+      ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      subtitulo: 'Haz solicitado',
+      titulo: 'Asistencia Médica',
+      color1: Color(0xff526bf6),
+      color2: Color(0xff67acf2),
     );
   }
 }
--- headers/lib/src/widgets/headers.dart
+++ headers/lib/src/widgets/headers.dart
@@ -296,25 +296,41 @@ HeaDER Emergency
  */
 
 class IconHeader extends StatelessWidget {
+
+  final IconData icon;
+  final String titulo;
+  final String subtitulo;
+  final Color color1;
+  final Color color2;
+
+
+  IconHeader({
+    @required this.icon,
+    @required this.titulo,
+    @required this.subtitulo,
+    this.color1 = Colors.grey,
+    this.color2 = Colors.blueGrey
+  });
+
   @override
   Widget build(BuildContext context) {
     final Color colorBlanco = Colors.white.withOpacity(0.7);
     return Stack(
       children: <Widget>[
-        _IconHeaderBackground(),
+        _IconHeaderBackground(color1: this.color1,color2: this.color2,),
         Positioned(
           top: -50,
           left: -70,
-          child: FaIcon(FontAwesomeIcons.plus, size: 250, color: Colors.white.withOpacity(0.2),)
+          child: FaIcon(this.icon, size: 250, color: Colors.white.withOpacity(0.2),)
         ),
         Column(
           children: <Widget>[
             SizedBox(height: 80,width: double.infinity,),
-            Text('Haz solicitado', style: TextStyle(fontSize: 20, color: colorBlanco)),
+            Text(this.subtitulo, style: TextStyle(fontSize: 20, color: colorBlanco)),
             SizedBox(height: 20),
-            Text('Asistencia Médica', style: TextStyle(fontSize: 25, color: colorBlanco, fontWeight: FontWeight.bold)),
+            Text(this.titulo, style: TextStyle(fontSize: 25, color: colorBlanco, fontWeight: FontWeight.bold)),
             SizedBox(height:20),
-            FaIcon(FontAwesomeIcons.plus, size: 80, color: Colors.white,)
+            FaIcon(this.icon, size: 80, color: Colors.white,)
           ],
         )
       ],
@@ -323,6 +339,14 @@ class IconHeader extends StatelessWidget {
 }
 
 class _IconHeaderBackground extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+
+  _IconHeaderBackground({
+    @required this.color1,
+    @required this.color2
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -334,7 +358,10 @@ class _IconHeaderBackground extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: <Color>[Color(0xff526bf6), Color(0xff67acf2),]
+          colors: <Color>[
+            this.color1,
+            this.color2
+          ]
         )
       ),
     );
