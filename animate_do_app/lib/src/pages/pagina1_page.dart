a6a77f031c6d4b421e6aae773695f4cf69a425ec
Kike Sanchez <kikin1601@hotmail.com>
2020-11-24 20:22:20 -0500

    animate_do

---
+++ animate_do_app/lib/src/pages/pagina1_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Pagina1_Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
