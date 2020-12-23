bcf209eace07789f8867cb88dd46adce6f70023a
Kike Sanchez <kikin1601@hotmail.com>
2020-12-23 12:32:10 -0500

    titulo y boton de play

--- musicPlayer/lib/src/pages/music_player_page.dart
+++ musicPlayer/lib/src/pages/music_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:musicPlayer/src/widgets/custom_appbar.dart';
 
 class MusicPlayerPage extends StatelessWidget {
@@ -8,7 +9,41 @@ class MusicPlayerPage extends StatelessWidget {
       body: Column(
         children: [
           CustomAppBar(),
-          ImagenDiscoDuracion()
+          ImagenDiscoDuracion(),
+          TituloPlay(),
+        ],
+      ),
+    );
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      margin: EdgeInsets.only(top: 40),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Nothing's gonna stop us now", style: TextStyle(fontSize: 18, color: Colors.white.withOpacity(0.8))),
+              Text("Starship", style: TextStyle(fontSize: 15, color: Colors.white.withOpacity(0.4))),
+
+            ],
+          ),
+          Spacer(),
+
+          FloatingActionButton(
+            elevation: 0,
+            highlightElevation: 0,
+            backgroundColor: Color(0xfff8cb51),
+            child: Icon(FontAwesomeIcons.play, size: 16),
+            onPressed: (){
+
+            },
+          )
         ],
       ),
     );
@@ -74,7 +109,8 @@ class ImagenDisco extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
-      width: 250, height: 250,
+      width: 250,
+      height: 250,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(200),
         child: Stack(
@@ -82,7 +118,8 @@ class ImagenDisco extends StatelessWidget {
           children: [
             Image(image: AssetImage('assets/cover.png')),
             Container(
-              width: 25, height: 25,
+              width: 25,
+              height: 25,
               decoration: BoxDecoration(
                 color: Colors.black38,
                 borderRadius: BorderRadius.circular(100)
