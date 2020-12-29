1174a262446244462bb6cf2602dd7e266f33ad0e
Kike Sanchez <kikin1601@hotmail.com>
2020-12-29 12:20:02 -0500

    transiciones

---
+++ transicion_app/lib/pages/pagina1_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:transicion_app/pages/pagina2_page.dart';
+
+class Pagina1Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página 1'),
+      ),
+      body: Center(
+        child: Text('Página 1'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.chevron_right),
+        onPressed: (){
+          Navigator.push(context, _crearRuta());
+        },
+      ),
+    );
+  }
+
+  Route _crearRuta() {
+    return PageRouteBuilder(
+      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => Pagina2Page(),
+      transitionDuration: Duration(milliseconds: 500),
+      transitionsBuilder: (context, animation, secondaryAnimation, child){
+        final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+        return SlideTransition(
+          child: FadeTransition(
+            child: child,
+            opacity: Tween<double>(begin: 0.0,end: 1.0)
+              .animate(curvedAnimation),
+          ),
+          position: Tween<Offset>(begin:Offset(0.0, 1.0),end: Offset.zero)
+              .animate(curvedAnimation),
+        );
+       /* return SlideTransition(
+            position: Tween<Offset>(begin:Offset(0.5,1.0),end: Offset.zero).animate(curvedAnimation),
+          child: child,
+        );*/
+
+        /*return ScaleTransition(
+          child: child,
+          scale: Tween<double>(begin: 0.0, end:1.0).animate(curvedAnimation)
+        );*/
+        
+        /*return RotationTransition(
+          child: child,
+          turns: Tween<double>(begin: 0.0,end: 1.0).animate(curvedAnimation)
+        );*/
+
+        /*return FadeTransition(
+          child: child,
+          opacity: Tween<double>(begin: 0.0,end: 1.0).animate(curvedAnimation)
+        );*/
+      }
+    );
+  }
+}
